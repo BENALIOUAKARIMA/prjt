268d8fc32433c5eb558b9ec5b384e03efb636d45
BENALIOUA Karima <karimabenalioua9@gmail.com>
2022-06-21 13:22:53 +0000

    health

--- views/register.php
+++ views/register.php
@@ -66,11 +66,11 @@ if (isset($_POST['submit'])) {
             
             if (document.getElementById('password').value != document.getElementById('pass').value) {
                 alert("Verifier le mot de passe");
-                 document.getElementById('button').disabled=true; //ila kan ghalat disactiver button
+                 document.getElementById('button').disabled=true; //error disactiver button
             }
             else
             {
-                document.getElementById('button').disabled=false; // s7i7 activer button
+                document.getElementById('button').disabled=false; // correct activer button
             }
 
 
@@ -79,4 +79,3 @@ if (isset($_POST['submit'])) {
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-
